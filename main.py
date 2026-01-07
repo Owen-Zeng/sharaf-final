66a88d395c0cc6daabfe7111a40eb548df3b0d3c
Alvin Cheng <144635388+overseeroverseer@users.noreply.github.com>
2026-01-07 15:34:51 -0500

    rlqriqccj
    Refactor simulation parameters and remove unused UI elements.

--- main.py
+++ main.py
@@ -1,81 +1,30 @@
 Web VPython 3.2
 
+edge1 = box(pos = vec(-100, 0 ,0), color = color.red, length = 10, height = 10, width = 10)
+edge2 = box(pos = vec(100, 0, 0), color = color.red, length = 10, height = 10, width = 10)
 
-buttonText = wtext(text = "\n Toggle Animation: ")
-animationStatus = wtext(text = "Off ")
-animationToggle = button(bind = toggleAnimation, text = "Toggle")
+numPlanks = float(input("Enter Number of Planks"))
 
-
-wt = wtext(text = "\nSelect Iterations: ")
-selectIterations = slider(bind= iterationsSlider, min = 5, max =10, step = 1, disabled = True)
-Iwt = wtext(text = "1")
-
-
-wt = wtext(text = "\nEdge Exclusion: ")
-edge = slider(bind = edgeSlider, min = 0, max = 45, step = 5)
-Ewt = wtext(text = edge.value + "%")
-
-swt = wtext(text = "\nStarting Seed Value: ")
-seedSlider = slider(bind=seedSliderFunc, min=0.05, max=0.95, step=0.01, value=0.1)
-seedWt = wtext(text="0.1")
-
-Swt = wtext(text = "\nStart Simulation   ")
-start = button(bind = toggleSim, text = "Start")
-
-def handMenu(evt):
-    return selectHand.value
-
-def iterationsSlider(evt):
-    Iwt.text = selectIteraions.value
-    return selectIterations.value
-
-def edgeSlider(evt):
-    return edge.value
-
-def seedSliderFunc(evt):
-    seedWt.text = str(round(seedSlider.value, 2))
-
-def toggleAnimation(evt):
-    global animationStatus
-    global animation
-
-    animation = not animation
-
-    if(animation):
-        animationStatus.text = "On "
-    else:
-        animationStatus.text = "Off "
-        
-def toggleSim(evt):
-    w = 10
-
-edge1 = box(pos = (-100, 0 ,0))
-edge2 = box(pos = (100, 0, 0))
-
-ball = sphere(pos = initPos, mass = 4, make_trail = True, radius = 0.5)
-
-
-gx = graph(title='vx vs Time', ytitle='velocity x', xtitle='Time')
-gy = graph(title='vy vs Time', ytitle='velocity y', xtitle='Time')
-
-cx = gcurve(graph = gx, color = color.blue)
-cy = gcurve(graph = gy, color = color.purple)
-
+#constants
 dt = 0.1 
 t = 1 
 g = vec(0, -9.81, 0)
+mass = 10
+k = 200
 
-b = float(input("Enter b"))
-v0 = 40
-theta = 35
+nugs = []
+fries = []
 
+for i in range(-100, 100, 200/numPlanks):
+    nugs.append(sphere(pos = vec(i+100/numPlanks, 0, 0), color = color.orange, radius = 50/numPlanks))
+    fries.append(box(pos = vector(i+100/numPlanks, 0, 0), color = color.yellow, length = 200/numPlanks))
 
-initVel = vec(v0 * cos(radians(theta)), v0 * sin(radians(theta)), 0)
-ball.velocity = initVel
+    
+equilPos.y = 0
+
 
 def acc(v, t):
-    return (ball.mass*g - b*v)/ball.mass
-    
+    return ((mass*g - b*v)/mass) + (-k * (ball.pos.y - equilPos)/mass)
     
 def pos(v, t):
     return v * dt
@@ -85,14 +34,13 @@ def RK2(f, X, t, dt):
     k2 = f(X + k1/2, t + dt/2) * dt
     return k2
     
-while ball.pos.y >= 0:
+while 1:
     rate(1/dt)
-    
-    ball.velocity += RK2(acc, ball.velocity, t, dt)
-    ball.pos += RK2(pos, ball.velocity, t, dt)
+    for nug in nugs:
+        nug.velocity += RK2(acc, nug.velocity, t, dt)
+        nug.pos += RK2(pos, nug.velocity, t, dt)
+    for fry in fries:
+        fry.velocity += RK2(acc, fry.velocity, t, dt)
+        fry.pos += RK2(pos, fry.velocity, t, dt)
     
     t += dt
-    
-    
-    cx.plot(t, ball.velocity.x)
-    cy.plot(t, ball.velocity.y)
