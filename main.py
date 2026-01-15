a56f2b9d31b22962b72217b982e2a79f13ec3919
Alvin Cheng <144635388+overseeroverseer@users.noreply.github.com>
2026-01-15 15:35:05 -0500

    Add run functionality and character mass variable

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ spring_force = 0
 numPlanks = 5
 character = ""
 setup = False
+run = False
+characterMass = 200
+
 
 characterList = ["Snowman", "Man", "Chicken"]
+
 
 
 select_spring_force = slider(bind =  select_k, step = 0.01)
@@ -40,10 +44,25 @@ def getChar(evt):
 
     character_text.text = "Character:" + character + "\n"
     return character
+    
+mykeys = keysdown()
+prose = label() 
+lastLen = len(prose.text)
 
+def keyInput(evt):
+    s = evt.key
+    if ( s == 'left' or s is 'right' ):
+        prose.text += s
+        
+
+scene.bind('keydown', keyInput)
 
 start_button = button(bind = start, text = "Start")
 setup_text = wtext(text = "Setup: False\n")
+
+run_button = button(bind = run, text = "Run")
+run_text = wtext(text = "Run: False\n")
+
 
 nugs = []
 fries = []
@@ -73,9 +92,18 @@ def start(evt):
         
     for nug in nugs:
         nug.velocity = vec(0,0,0)
+        nug.mass = 1500
     
     for fry in fries:
         fry.velocity = vec(0,0,0)
+    
+def run(evt):
+    if run:
+        run_button.text = "Stop"
+    else:
+        run_button.text = "Start"
+    run_text.text = "Run: " + str(run) + "\n"
+        
         
 def springAcc(nug, X):
     return ((-k * (nug.pos - X)) - b*nug.velocity)/mass
@@ -96,7 +124,18 @@ def RK2(f, X, t, dt):
 while 1:
     rate(500)
     if(setup):
+        if run:
+            curr = -1
+            if (lastLen is not len(prose.text)):
+                if prose.text[len(prose.text)-1] is right and curr < numPlanks-1:
+                    curr+=1
+                if prose.text[len(prose.text)-1] is left and curr > 0:
+                    curr-=1
+                lastLen = len(prose.text)
+            if curr >= 0:
+                nugs[i].mass += characterMass
         for i in range(numPlanks):
+            mass = nug[i].mass
             if (i == 0):
                 leftAcc = springAcc(nugs[i], edge1.pos)
                 rightAcc = springAcc(nugs[i], nugs[i+1].pos)
@@ -124,5 +163,6 @@ while 1:
             fries[i].pos = center
             fries[i].axis = axis
             fries[i].length = mag(axis)
+        
         
         t += dt 
