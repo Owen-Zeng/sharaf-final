c64f818e56f352345d9ee7f3542823ec34eb90ae
Matthew Cheng <mcheng60@stuy.edu>
2026-01-16 00:56:05 -0500

    Snowman and bigmac changes

--- design.py
+++ design.py
@@ -1,11 +1,14 @@
 def snowman():
     sphere(pos = vec(-100,10,0), color = color.white, radius = 5)
     sphere(pos = vec(-100,18,0), color = color.white, radius = 4)
-    box(pos = vec(-97,11,0), axis = vec(.3,0.35,0), length = 13, color = color.green)
-    box(pos = vec(-103,11,0), axis = vec(-.3,.35, 0), length = 13, color = color.green)
+    box(pos = vec(-97,11,0), axis = vec(.3,0.35,0), length = 13, color = vec(0.531,0.318,.161))
+    box(pos = vec(-103,11,0), axis = vec(-.3,.35, 0), length = 13, color = vec(0.531,0.318,.161))
     arrow(pos = vec(-100,17,2), axis = vec(0, 0, 1), length = 4, color = color.orange)
-    sphere(pos = vec(-101,19,3.2), color = color.orange, radius = 1.3)
-    sphere(pos = vec(-99,19,3.2), color = color.orange, radius = 1.3)
+    sphere(pos = vec(-101,19,3.2), color = color.black, radius = 1.3)
+    sphere(pos = vec(-99,19,3.2), color = color.black, radius = 1.3)
+    sphere(pos = vec(-100,9,4.5), color = color.black, radius = 0.8)
+    sphere(pos = vec(-100,10.5,4.6), color = color.black, radius = 0.8)
+    sphere(pos = vec(-100,12,3.9), color = color.black, radius = 0.8)
     
     
 
@@ -15,20 +18,19 @@ def snowman():
 for i in range(-100, 100, 200/numPlanks):
     nugs.append(cylinder(pos = vec(i+100/numPlanks, -10/numPlanks, 0), color = color.orange, axis = vec(0,1,0), size = vec(15/numPlanks, 60/numPlanks, 50/numPlanks), texture = textures.rough, emissive = True))
     
-    
-#L up
-#H Sideways
-#W In out
+#Length y
+#Height x
+#Width z
 def macBig():
     cylinder(pos = vec(-100,5.1,0), axis = vec(0,1,0), height = 5, length = 0.2, width = 5, color = color.orange) #bottom bun
-    cylinder(pos = vec(-100,5.3,0), axis = vec(0,1,0), height = 5, length = .2, width = 5, color = color.black) #bottom patty
+    cylinder(pos = vec(-100,5.3,0), axis = vec(0,1,0), height = 5, length = .2, width = 5, color = vec(0.531,0.318,.161)) #bottom patty
     cylinder(pos = vec(-100,5.5,0), axis = vec(0,1,0), height = 5, length = .1, width = 5, color = color.yellow) #bottom cheese
     cylinder(pos = vec(-100,5.6,0), axis = vec(0,1,0), height = 5, length = .1, width = 5, color = color.green) #bottom lettuce
     cylinder(pos = vec(-100,5.7,0), axis = vec(0,1,0), height = 5, length = .5, width = 5, color = color.orange) #middle bun
-    cylinder(pos = vec(-100,6.2,0), axis = vec(0,1,0), height = 5, length = .2, width = 5, color = color.black) #top patty
+    cylinder(pos = vec(-100,6.2,0), axis = vec(0,1,0), height = 5, length = .2, width = 5, color = vec(0.531,0.318,.161)) #top patty
     cylinder(pos = vec(-100,6.4,0), axis = vec(0,1,0), height = 5, length = .1, width = 5, color = color.yellow) #top cheese
     cylinder(pos = vec(-100,6.5,0), axis = vec(0,1,0), height = 5, length = .1, width = 5, color = color.green) #top lettuce
-    sphere(pos = vec(-100,6.45,0), axis = vec(0,1,0), radius = 2.3, color = color.orange) #top bun
+    sphere(pos = vec(-100,6.7,0), size = vec(5,1.5,5), color = color.orange,) #top bun
 #macBig()
     
 def chicken():
@@ -42,3 +44,4 @@ def chicken():
     sphere(pos = vec(-95.5,16.5,-1), color = color.orange, radius = 1.3)
     arrow(pos = vec(-95.2,16,0), length = 3, color = color.orange)
 #chicken()
+    
